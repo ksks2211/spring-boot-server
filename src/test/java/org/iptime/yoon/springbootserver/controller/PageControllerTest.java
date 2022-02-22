0e6f4a527d0cb46540c2f03cf64b1bd9b7ba2f4c
ksks2211 <rival15@naver.com>
2022-02-22 14:17:56 +0900

    enabled h2-console

--- src/test/java/org/iptime/yoon/springbootserver/controller/PageControllerTest.java
+++ src/test/java/org/iptime/yoon/springbootserver/controller/PageControllerTest.java
@@ -1,7 +1,32 @@
+package org.iptime.yoon.springbootserver.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * @author rival
  * @since 2022-02-22
- */class PageControllerTest {
-  
+ */
+@WebMvcTest(controllers = PageController.class)
+class PageControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void main_page() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/test"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("test"));
+    }
+
 }
